5e243839ab713d1a1cd18abd3dbae8ca74261d03
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-16 22:16:53 +0300

    chore: testing

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 // Connect to Database
 connectDB();
 /*------------------------------------------------------ */
-
+app.use(express.json());
 app.post("/books", async (req, res) => {
   const book = req.body;
 
@@ -17,6 +17,14 @@ app.post("/books", async (req, res) => {
       .json({ error: true, message: "Please provide all fields" });
   }
   const newBook = new Book(book);
+
+  try {
+    await newBook.save();
+    res.status(201).json({ error: false, data: newBook });
+  } catch (error) {
+    console.error("Error: ", error.message);
+    res.status(500).json({ error: true, message: "Server Error" });
+  }
 });
 
 app.listen(5000, () => {
