c3cc9d5e9aa4c65b4e0de678b88204c717c88553
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-16 22:38:56 +0300

    feat: get & post & delete methods

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,26 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import Book from "./models/book.js";
 const app = express();
+app.use(express.json());
 /*------------------------------------------------------ */
 // Connect to Database
 connectDB();
 /*------------------------------------------------------ */
-app.use(express.json());
+
+/*------------------------------------------------------ */
+//Get
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json({ error: false, data: books });
+  } catch (error) {
+    console.error("Error: ", error.message);
+    res.status(500).json({ error: true, message: "Server Error" });
+  }
+});
+
+/*------------------------------------------------------ */
+//Post
 app.post("/books", async (req, res) => {
   const book = req.body;
 
@@ -26,6 +41,20 @@ app.post("/books", async (req, res) => {
     res.status(500).json({ error: true, message: "Server Error" });
   }
 });
+/*------------------------------------------------------ */
+
+/*------------------------------------------------------ */
+//Delete
+app.delete("/books/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Book.findByIdAndDelete(id);
+    res.status(200).json({ error: false, message: "Book deleted" });
+  } catch (error) {
+    res.status(404).json({ error: true, message: "Book not found" });
+  }
+});
+/*------------------------------------------------------ */
 
 app.listen(5000, () => {
   console.log("Server start: http://localhost:5000");
